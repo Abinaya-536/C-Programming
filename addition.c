efca41cee0b463de433e3e357e64d49bc6cc5165
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-25 18:18:36 +0530

    Add files via upload
    addtion in c

---
+++ addition.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+ int a =5;   
+ int b=5; 
+ int c=a+b;
+
+printf("%d\n",c);
+int aa =3;   
+ int bb=7; 
+ int cc=aa+bb;
+printf("%d\n",cc);
+int aaa =8;   
+ int bbb=2; 
+ int ccc=aaa+bbb;
+printf("%d",ccc);
+
+return 0;
+ 
+    
+}
