ba06d2a6d7f7faf0daa63b83b200cee993fe5e87
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-25 18:29:19 +0530

    Add files via upload
    subtract.c

---
+++ subtract.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+ int a =7;   
+ int b=2; 
+ int c=a-b;
+
+printf("%d\n",c);
+int aa =30;   
+ int bb=20; 
+ int cc=aa-bb;
+printf("%d\n",cc);
+
+
+return 0;
+ 
+    
+}
