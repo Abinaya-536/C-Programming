8457b0163625fbd343d0cdb11ca98e2110f10585
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-25 18:29:52 +0530

    Add files via upload
    multiply.c

---
+++ multiply.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+ int a =10;   
+ int b=2; 
+ int c=a*b;
+
+printf("%d\n",c);
+int aa =10;   
+ int bb=5; 
+ int cc=aa*bb;
+printf("%d\n",cc);
+
+
+return 0;
+ 
+    
+}
