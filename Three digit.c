d25d8b787ec972187a2ef04ec02ebd69d0a83797
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-26 22:39:24 +0530

    Add files via upload
    three digit int.c

---
+++ Three digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &num);
+
+    if (n >= 100 && n <= 999)
+        printf("Three-digit no");
+    else
+        printf("Not a three-digit no");
+
+    return 0;
+}
