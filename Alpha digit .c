d2834043c711e22f522c5edace2afef634f0530b
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-26 22:24:12 +0530

    Add files via upload
    Alpha.c

---
+++ Alpha digit .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c", &a);
+
+    if (a >= '0' && a <= '9')
+        printf("Digit");
+    else if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z'))
+        printf("Alphabet");
+    else
+        printf("Special Character");
+
+    return 0;
+}
