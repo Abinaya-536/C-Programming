21022c34a1ab4b00420295c5a4d46fa1215cbb61
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-01-26 22:30:44 +0530

    Add files via upload
    Even or odd.c

---
+++ even or odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if (a%2==0)
+        printf("Positive number");
+    else if (num < 0)
+        printf("Negative number");
+    else
+        printf("Zero");
+
+    return 0;
+}
