6b697ba6b7e6e52018899463e7b4a2ab2a0e8fa6
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-01 21:20:23 +0530

    Add files via upload
    solved for problems in c

---
+++ for3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, units;
+    int total = 0;
+
+    
+    scanf("%d", &n);
+
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &units);
+        total = total + units;
+    }
+
+    
+    printf("Total Units: %d", total);
+
+    return 0;
+}
