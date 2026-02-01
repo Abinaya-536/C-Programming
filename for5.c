af6665da567b98f1ba1609169b3df197afa6b2ac
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-01 21:23:12 +0530

    Add files via upload
    solved for problems in c

---
+++ for5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int battery, n, drain;
+
+    
+    scanf("%d", &battery);
+
+    
+    scanf("%d", &n);
+
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &drain);
+        battery = battery - drain;
+    }
+
+   
+    printf("Remaining Battery: %d%%", battery);
+
+    return 0;
+}
