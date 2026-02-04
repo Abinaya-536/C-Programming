03d24529f1aedcd4e2e4c9510f42388639e14294
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-04 22:37:31 +0530

    Add files via upload
    solved array problems in c

---
+++ arr3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    scanf("%d", &size);     
+    int arr[size];        
+
+       for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sorted = 1;           
+
+        for(int i = 0; i < size - 1; i++) {
+        if(arr[i] > arr[i + 1]) {
+            sorted = 0;       
+            break;                  }
+    }
+
+    if(sorted) {
+        printf("Yes\n");     
+    } else {
+        printf("No\n");           }
+
+    return 0;
+}
