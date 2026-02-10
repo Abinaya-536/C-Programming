c7db51aac9f50a17c3683f6645ac22482cfb8f81
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-10 20:31:43 +0530

    Add files via upload
    Solved 2D array problems in C

---
+++ 2D-Arrays-C/2D-Arrays/2Darray22.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int mat[R][C];
+
+     for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    int count = 0;
+
+     for (int i = 0; i < R; i++) {
+        int isIncreasing = 1;
+
+        for (int j = 1; j < C; j++) {
+            if (mat[i][j] <= mat[i][j - 1]) {
+                isIncreasing = 0;
+                break;
+            }
+        }
+
+        if (isIncreasing) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
