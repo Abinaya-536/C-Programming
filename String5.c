bf24ab2bedaaf0a03382633f8759b7f3e76bd781
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-12 22:26:43 +0530

    Add files via upload
    Solved string problem in C

---
+++ String5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, len;
+
+    printf("Enter the product code: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+      for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - 1 - i]) {
+            printf("Not Palindrome\n");
+            return 0; // Exit program immediately
+        }
+    }
+
+     printf("Palindrome\n");
+
+    return 0;
+}
