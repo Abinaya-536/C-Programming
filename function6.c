a2394ffe751f6a7bc48e1dc99221f998172307f0
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-14 22:09:24 +0530

    Add files via upload
    solved function problems in C

---
+++ function6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    long long fact = 1;
+    int i;
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    long long result = factorial(N);
+
+    printf("%lld", result);
+
+    return 0;
+}
