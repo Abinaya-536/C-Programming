1629415797705defcc2136e16985f325b60b499e
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-02-19 22:40:11 +0530

    Add files via upload
    Solved java problems

---
+++ Basic8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WeeklyWage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int daysWorked = sc.nextInt();
+        int wagePerDay = sc.nextInt();
+        int overtimeBonus = sc.nextInt();
+        int maintenanceCharge = sc.nextInt();
+
+        int baseSalary = daysWorked * wagePerDay;
+        int finalWage = baseSalary + overtimeBonus - maintenanceCharge;
+
+        System.out.println("Final Weekly Wage = " + finalWage);
+    }
+}
